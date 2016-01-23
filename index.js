dd60d1c1f2ff5a9dd2bb4e91bd59cd321cd3e165
Ivan Nikolić <niksy5@gmail.com>
2016-01-23 23:26:50 +0100

    Allow fetching multiple dates via humanized dates

--- index.js
+++ index.js
@@ -3,30 +3,49 @@ var JSONStream = require('JSONStream');
 var through = require('through2');
 var multistream = require('multistream');
 var request = require('request');
-var moment = require('moment');
+var formatDate = require('date-fns/format');
 var parse = require('./lib/parse');
+var humanizedDate = require('./lib/humanized-date');
 var shows = require('./tv-shows.json');
 var sources = require('./sources.json');
 
 /**
  * @param  {Object[]} sources
+ * @param  {Date[]} dates
  *
  * @return {Array}
  */
-function getShowsFromSources ( sources ) {
-	var yesterday = moment().add(-1, 'days').format('YYYY-MM-DD');
-	var arr = [];
-	_.each(sources, function ( source ) {
-		if ( source.type !== 'hrt' ) {
-			arr.push(request(source.url.replace('{{ YYYY-MM-DD }}', yesterday)));
-		}
-	});
-	return arr;
+function getShows ( sources, dates ) {
+
+	return _.chain(sources)
+		.map(function ( source ) {
+			if ( source.type === 'hrt' ) {
+				return null;
+			}
+			return _.map(dates, function ( date ) {
+				return request(_.template(source.url)({ date: formatDate(date, 'YYYY-MM-DD') }));
+			});
+		})
+		.compact()
+		.flatten()
+		.value();
+
 }
 
-module.exports = function () {
+/**
+ * @param  {Date[]} dates
+ *
+ * @return {Stream}
+ */
+module.exports = function ( dates ) {
 
-	return multistream(getShowsFromSources(sources))
+	dates = _.compact([].concat(dates));
+
+	if ( !dates.length ) {
+		throw new Error('Date isn’t provided.');
+	}
+
+	return multistream(getShows(sources, dates))
 		.pipe(JSONStream.parse('*'))
 		.pipe(through.obj(function ( data, enc, end ) {
 			var parsed = parse({
