3808e00ed5529f5b5c40b49518ea652d370887e5
Ivan Nikolić <niksy5@gmail.com>
2016-01-23 23:27:11 +0100

    Add CLI interface and interactive prompt

--- cli.js
+++ cli.js
@@ -2,9 +2,74 @@
 
 var JSONStream = require('JSONStream');
 var format = require('format-json-stream');
+var meow = require('meow');
+var elegantSpinner = require('elegant-spinner');
+var logUpdate = require('log-update');
+var humanizedDate = require('./lib/humanized-date');
+var interactivePrompt = require('./lib/interactive-prompt');
 var shows = require('./');
+var fetchedShows = [];
+var stream, cli;
 
-shows()
-	.pipe(JSONStream.stringify())
-	.pipe(format())
-	.pipe(process.stdout);
+var spinner = {
+	frame: elegantSpinner(),
+	start: function () {
+		this._spin = setInterval(function () {
+			logUpdate(this.frame());
+		}.bind(this), 50);
+	},
+	stop: function () {
+		clearInterval(spinner._spin);
+	}
+};
+
+
+process.stdin.on('keypress', function ( ch, key ) {
+	if ( key && key.name === 'escape' ) {
+		process.exit();
+	}
+});
+
+cli = meow([
+	'Usage',
+	'  $ tv-shows',
+	'',
+	'Options',
+	'  -o, --output-json  Output results as JSON',
+	'  -d, --date [human date]  Display TV shows for given date(s)',
+].join('\n'), {
+	alias: {
+		o: 'output-json',
+		d: 'date',
+		v: 'version',
+		h: 'help'
+	}
+});
+
+stream = shows(humanizedDate(cli.flags.date || 'yesterday'));
+
+spinner.start();
+stream
+	.on('end', function () {
+		spinner.stop();
+		logUpdate.clear();
+	});
+
+if ( cli.flags.outputJson ) {
+
+	stream
+		.pipe(JSONStream.stringify())
+		.pipe(format())
+		.pipe(process.stdout);
+
+} else {
+
+	stream
+		.on('data', function ( data ) {
+			fetchedShows.push(data);
+		})
+		.on('end', function () {
+			interactivePrompt(fetchedShows);
+		});
+
+}
